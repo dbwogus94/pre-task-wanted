7a6405c2ee3630f9fb9abcc9b66fffb205bb0c35
jae hyun <1994dbwogus@gmail.com>
2024-09-11 01:44:42 +0900

    feat: PostModule에 신규 providers 정의

--- src/domain/post/post.repository.ts
+++ src/domain/post/post.repository.ts
@@ -1,1 +1,18 @@
-export class PostRepository {}
+import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
+
+import { BaseRepository } from '@app/common';
+import { PostEntity } from '@app/entity';
+
+export abstract class PostRepositoryPort extends BaseRepository<PostEntity> {}
+
+@Injectable()
+export class PostRepository extends PostRepositoryPort {
+  constructor(
+    @InjectEntityManager()
+    manager: EntityManager,
+  ) {
+    super(PostEntity, manager);
+  }
+}
--- src/domain/post/post.service.ts
+++ src/domain/post/post.service.ts
@@ -1,4 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  CreatePostRequest,
+  DeletePostRequest,
+  GetPostResponse,
+  GetPostsQuery,
+  GetPostsResponseWithTotalCount,
+  PutPostRequest,
+} from './dto';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { PostRepositoryPort } from './post.repository';
+
+export abstract class PostServiceUseCase {
+  abstract getPosts(
+    query: GetPostsQuery,
+  ): Promise<GetPostsResponseWithTotalCount>;
+  abstract createPost(body: CreatePostRequest): Promise<void>;
+  abstract getPost(postId: string): Promise<GetPostResponse>;
+  abstract updatePost(postId: string, body: PutPostRequest): Promise<void>;
+  abstract softDeletePost(
+    postId: string,
+    body: DeletePostRequest,
+  ): Promise<void>;
+}
 
 @Injectable()
-export class PostService {}
+export class PostService extends PostServiceUseCase {
+  constructor(
+    @InjectDataSource()
+    private readonly dataSource: DataSource,
+    private readonly postRepo: PostRepositoryPort,
+  ) {
+    super();
+  }
+
+  async getPosts(
+    query: GetPostsQuery,
+  ): Promise<GetPostsResponseWithTotalCount> {
+    throw new NotFoundException('미구현 API');
+  }
+
+  async createPost(body: CreatePostRequest): Promise<void> {
+    throw new NotFoundException('미구현 API');
+  }
+
+  async getPost(postId: string): Promise<GetPostResponse> {
+    throw new NotFoundException('미구현 API');
+  }
+
+  async updatePost(postId: string, body: PutPostRequest): Promise<void> {
+    throw new NotFoundException('미구현 API');
+  }
+
+  async softDeletePost(postId: string, body: DeletePostRequest): Promise<void> {
+    throw new NotFoundException('미구현 API');
+  }
+}
