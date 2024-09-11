a5d0957735c3257d81230ef17bf5e539be04ad0a
jae hyun <1994dbwogus@gmail.com>
2024-09-11 17:48:55 +0900

    feat: PostModule, CommentModule 순환 의존성 세팅

--- src/domain/comment/comment.module.ts
+++ src/domain/comment/comment.module.ts
@@ -1,15 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { CommentController } from './comment.controller';
 import { CommentService, CommentServiceUseCase } from './comment.service';
 import { CommentRepository, CommentRepositoryPort } from './comment.repository';
 import { PostModule } from '../post/post.module';
 
 @Module({
-  imports: [PostModule],
+  imports: [forwardRef(() => PostModule)],
   controllers: [CommentController],
   providers: [
     { provide: CommentServiceUseCase, useClass: CommentService },
     { provide: CommentRepositoryPort, useClass: CommentRepository },
   ],
+  exports: [CommentRepositoryPort],
 })
 export class CommentModule {}
--- src/domain/post/post.module.ts
+++ src/domain/post/post.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService, PostServiceUseCase } from './post.service';
 import { PostRepository, PostRepositoryPort } from './post.repository';
+import { CommentModule } from '../comment/comment.module';
 
 @Module({
+  imports: [forwardRef(() => CommentModule)],
   controllers: [PostController],
   providers: [
     { provide: PostServiceUseCase, useClass: PostService },
